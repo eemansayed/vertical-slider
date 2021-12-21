c9936f8021ea882b812fe0dec404b94fb5718719
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-21 09:49:07 +0000

    add const called upButton

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const sliderContainer = document.querySelector(".slider-container");
 const slideRight = document.querySelector(".right-slide");
 const slideLeft = document.querySelector(".left-slide");
-const sliderContainer = document.querySelector(".slider-container");
+const upButton = document.querySelector(".up-button");
 const sliderContainer = document.querySelector(".slider-container");
 const sliderContainer = document.querySelector(".slider-container");
