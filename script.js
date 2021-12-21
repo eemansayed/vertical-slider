2b3a2af4af024c20a105f7998d21f84836ee95f9
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-21 09:50:56 +0000

    add const called slideLength

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const sliderContainer = document.querySelector(".slider-container");
 const slideRight = document.querySelector(".right-slide");
 const slideLeft = document.querySelector(".left-slide");
 const upButton = document.querySelector(".up-button");
-const sliderContainer = document.querySelector(".slider-container");
-const sliderContainer = document.querySelector(".slider-container");
+const downButton = document.querySelector(".down-button");
+const sliderLength = document.querySelectorAll(".slider-container");
