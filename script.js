e0c43a032cf0732e52eb80aa7ac23e68b31b9921
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-21 09:47:34 +0000

    add const called slideRight

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const sliderContainer = document.querySelector(".slider-container");
+const slideRight = document.querySelector(".right-slide");
+const sliderContainer = document.querySelector(".slider-container");
+const sliderContainer = document.querySelector(".slider-container");
+const sliderContainer = document.querySelector(".slider-container");
+const sliderContainer = document.querySelector(".slider-container");
